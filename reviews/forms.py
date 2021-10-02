a9285e9167887c27e4dd0477a076b2493e4c05bd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-02 12:49:29 +0200

    feat: setup and configure moderlform

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,16 +1,32 @@
 from django import forms
+from .models import Review
 
-
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="Your name",
-        max_length=100,
-        error_messages={
-            "required": "Your name must not be empty.",
-            "max_length": "Please enter a shorter name.",
-        },
-    )
-    review_text = forms.CharField(
-        label="Your Feedback", widget=forms.Textarea, max_length=200
-    )
-    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(
+#         label="Your name",
+#         max_length=100,
+#         error_messages={
+#             "required": "Your name must not be empty.",
+#             "max_length": "Please enter a shorter name.",
+#         },
+#     )
+#     review_text = forms.CharField(
+#         label="Your Feedback", widget=forms.Textarea, max_length=200
+#     )
+#     rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+        # exclude = ['field_to_exclude']
+        labels = {
+            "user_name": "Your name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating",
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty.",
+                "max_length": "Please enter a shorter name.",
+            }
+        }
