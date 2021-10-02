091b8342dfe4dc5a284157d50ab8a8cd66ad50fd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-02 15:56:18 +0200

    feat: class based views

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.review), path("thank-you", views.thank_you)]
+urlpatterns = [
+    path("", views.ReviewView.as_view()),
+    path("thank-you", views.thank_you),
+]
