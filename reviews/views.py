325656b254e81fa6be7095a9b4fccc6bd98c455b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-03 11:58:24 +0200

    feat: use templateview

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -20,18 +21,10 @@ class ReviewView(View):
         return render(request, "reviews/review.html", {"form": form})
 
 
-# def review(request):
-#     if request.method == "POST":
-#         form = ReviewForm(request.POST)
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect("/thank-you")
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, "reviews/review.html", {"form": form})
-
-
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
