c6b8b508daef325cd3641e4d36404a5a0984b3fa
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-10-04 16:55:34 +0200

    feat: serve uploaded files

--- feedback/settings.py
+++ feedback/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = "/static/"
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 
+MEDIA_URL = "/user-media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
